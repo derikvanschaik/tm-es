929f7220fd771c6343df2fdbdee8e1c323313f13
Derik van Schaik <derikvanschaik@gmail.com>
2024-08-30 23:28:18 -0700

    make it work for github deploy

--- _config.ts
+++ _config.ts
@@ -1,6 +1,10 @@
 import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 
-const site = lume();
+const site = lume({
+  location: new URL("https://derikvanschaik.github.io/tm-es/"),
+});
+site.use(basePath());
 
 site.copy("/styles.css");
 site.copy("assets");
